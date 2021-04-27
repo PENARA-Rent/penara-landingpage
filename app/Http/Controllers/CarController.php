c6bf7ea2bc4185d44821b4757eee0838460a30a6
januarelsan <januarelsan@gmail.com>
2021-04-28 01:04:59 +0700

    Update Car Controller

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -27,11 +27,90 @@ class CarController extends Controller
         $allCarCount = Car::all()->count();
         $brands = Brand::all();      
         
-        return view('pages.main.car-list', compact('cars','brands','allCarCount','brandId'));
-    }    
-
+        return view('pages.main.car.list', compact('cars','brands','allCarCount','brandId'));
+    }  
+    
+    
     public function detail($id){        
         $car = Car::findOrFail($id);                
-        return view('pages.main.car-detail', compact('car'));
+        return view('pages.main.car.detail', compact('car'));
+    }
+    
+    public function adminList(){                
+        $cars = Car::all();                
+        return view('pages.admin.car.list', compact('cars'));
+    }  
+    public function addForm(){                
+        $brands = Brand::all();
+        return view('pages.admin.car.addForm', compact('brands'));
+    }
+
+    public function adminDetail($id){                
+        $brands = Brand::all();
+        $car = Car::findOrFail($id);
+        return view('pages.admin.car.detail', compact('brands','car'));
+    }
+
+    public function edit(Request $request){  
+        
+        $validated = $request->validate([
+            'brand_id' => 'required|integer',
+            'name' => 'required',            
+            'description' => 'required',
+            'price' => 'required|integer|between:1,10000000',            
+        ]);
+
+        $brands = Brand::all();                      
+        $car = Car::find($request->id);
+
+        // return dd($car);
+
+        $data = [            
+            'brand_id' => $request->brand_id,
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+        ];
+        
+        $car->brand_id = $request->brand_id;
+        $car->name = $request->name;
+        $car->description = $request->description;
+        $car->price = $request->price;
+        $car->save();
+        
+        return redirect()->back()->with('success', 'Car was Successfully Updated :)');   
+        // return view('pages.admin.car.detail', compact('brands','car'));
+    }
+
+    public function store(Request $request){        
+        $validated = $request->validate([
+            'brand_id' => 'required|integer',
+            'name' => 'required',            
+            'description' => 'required',
+            'price' => 'required|integer|between:1,10000000',            
+        ]);
+            
+        $data = [            
+            'brand_id' => $request->brand_id,
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+        ];
+
+        
+        
+        $car = Car::create($data);
+
+        $data = [            
+            'car_id' => $car->id,
+            'path' => "gauto/assets/img/offer-toyota.png"                   
+        ];
+
+        $carImage = CarImage::create($data);
+        
+
+        // return dd($car);
+        return redirect()->route('admin.car.list')->with('success', 'Car was Successfully Added :)');   
+        // return redirect()->back()->with('message','Car was Successfully Added :)');   
     }
 }
