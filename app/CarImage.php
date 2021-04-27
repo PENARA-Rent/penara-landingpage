4abf178932bf62694044759cf372d531f9fa88a6
januarelsan <januarelsan@gmail.com>
2021-04-28 01:04:34 +0700

    Add Fillable Var

--- app/CarImage.php
+++ app/CarImage.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class CarImage extends Model
 {
     //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'car_id', 'path'
+    ];
     public function car()
     {
         return $this->belongsTo(Car::class);
